05834d75873675207aececd0956be75c7910e471
Rosaida <rosaidabhz@gmail.com>
2023-01-14 22:27:02 -0500

    maquinas

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,67 +8,60 @@ export class ProductosService {
   products: Maquinas[] = [
     {
       id: 1,
-      title: 'Hamburguesa de Res',
-      caption: '450gr / 5 Unidades',
-      price: 7900,
-      img: '../../../assets/img/res.png'
+      title: 'Maquina manual',
+      caption: 'COD: SB04A',
+      price: 120,
+      img: '/assets/product_images/1.png'
     },
     {
       id: 2,
-      title: 'Hamburguesa de Pollo',
-      caption: '450gr / 5 Unidades',
-      price: 7900,
-      img: '../../../assets/img/pollo.png'
+      title: 'Maquina Manual',
+      caption: 'COD: ST-130',
+      price: 140,
+      img: '/assets/product_images/2.png'
     },
     {
       id: 3,
-      title: 'Salchicha Perro',
-      caption: '500gr / 12 Unidades',
-      price: 8350,
-      img: '../../../assets/img/salchicha.png'
+      title: 'Maquina Doble Estacion',
+      caption: 'COD: ST-210',
+      price: 170,
+      img: '/assets/product_images/3.png'
     },
     {
       id: 4,
-      title: 'Mortadela de Res',
-      caption: '250gr / 12 Unidades',
-      price: 3700,
-      img: '../../../assets/img/mortadela.png'
+      title: 'Maquina Multifuncional 8-1',
+      caption: 'COD: ST-220',
+      price:350,
+      img: '/assets/product_images/4.png'
     },
     {
       id: 5,
-      title: 'Chorizo Méxicano',
-      caption: '450gr / 5 Unidades o 10 Unidades',
-      price: 11400,
-      img: '../../../assets/img/chorizo.png'
+      title: 'Maquina Multifuncional 8-1',
+      caption: 'COD: ST-220',
+      price: 390,
+      img: '/assets/product_images/5.png'
     },
     {
       id: 6,
-      title: 'Chorizo Cóctel',
-      caption: '250gr / 14 Unidades',
-      price: 7000,
-      img: '../../../assets/img/coctel.png'
+      title: 'Maquina Multifuncional 11-1',
+      caption: 'COD: P8200-V11',
+      price: 490,
+      img: '/assets/product_images/6.png'
     },
     {
       id: 7,
-      title: 'Jamón de Cerdo Ahumado',
-      caption: '250gr',
-      price: 7150,
-      img: '../../../assets/img/jamon-ahumado.png'
+      title: 'Maquina Multifuncional 5-1',
+      caption: 'COD: SB04W',
+      price: 420,
+      img: '/assets/product_images/7.png'
     },
     {
       id: 8,
-      title: 'Jamón Sándwich',
-      caption: '250gr',
-      price: 5200,
-      img: '../../../assets/img/jamon-sandwich.png'
+      title: 'Maquina de Gorras 2-1',
+      caption: 'Resistencia cuadrada',
+      price: 260,
+      img: '/assets/product_images/8.png'
     },
-    {
-      id: 9,
-      title: 'Pernil de Cerdo',
-      caption: '250gr',
-      price: 19200,
-      img: '../../../assets/img/pernil.png'
-    }
   ];
 
   
